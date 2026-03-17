febae84cd671539d02667ec1a72d8a2d2a720681
Meghal09 <79741511+Meghal09@users.noreply.github.com>
2026-03-17 05:35:31 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import TradeLedger from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <TradeLedger />
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<TradeLedger />)
