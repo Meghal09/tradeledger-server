7f6fa23e40595b695bc13aedba4856be107047ce
Meghal09 <79741511+Meghal09@users.noreply.github.com>
2026-03-17 05:19:49 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TradeLedger from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <TradeLedger />
+)
